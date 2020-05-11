4c95621471a70f60bdac92c9aa6e6df1d4d18556
davidatbu <davidat@bu.edu>
2020-05-11 05:48:41 -0400

    speed improvement from github issue

--- layers.py
+++ layers.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class GraphAttentionLayer(nn.Module):
+class GraphAttentionLayer(nn.Module[torch.Tensor]):
     """
     Simple GAT layer, similar to https://arxiv.org/abs/1710.10903
     """
@@ -17,10 +17,40 @@ class GraphAttentionLayer(nn.Module):
         self.alpha = alpha
         self.concat = concat
 
-        self.W = nn.Parameter(torch.zeros(size=(in_features, out_features)))
-        nn.init.xavier_uniform_(self.W.data, gain=1.414)
-        self.a = nn.Parameter(torch.zeros(size=(2*out_features, 1)))
-        nn.init.xavier_uniform_(self.a.data, gain=1.414)
+        self.W = nn.Parameter(
+            nn.init.xavier_uniform(
+                torch.Tensor(in_features, out_features).type(
+                    torch.cuda.FloatTensor
+                    if torch.cuda.is_available()
+                    else torch.FloatTensor
+                ),
+                gain=np.sqrt(2.0),
+            ),
+            requires_grad=True,
+        )
+        # self.a = nn.Parameter(nn.init.xavier_uniform(torch.Tensor(2*out_features, 1).type(torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor), gain=np.sqrt(2.0)), requires_grad=True)
+        self.a1 = nn.Parameter(
+            nn.init.xavier_uniform(
+                torch.Tensor(out_features, 1).type(
+                    torch.cuda.FloatTensor
+                    if torch.cuda.is_available()
+                    else torch.FloatTensor
+                ),
+                gain=np.sqrt(2.0),
+            ),
+            requires_grad=True,
+        )
+        self.a2 = nn.Parameter(
+            nn.init.xavier_uniform(
+                torch.Tensor(out_features, 1).type(
+                    torch.cuda.FloatTensor
+                    if torch.cuda.is_available()
+                    else torch.FloatTensor
+                ),
+                gain=np.sqrt(2.0),
+            ),
+            requires_grad=True,
+        )
 
         self.leakyrelu = nn.LeakyReLU(self.alpha)
 
@@ -28,10 +58,13 @@ class GraphAttentionLayer(nn.Module):
         h = torch.mm(input, self.W)
         N = h.size()[0]
 
-        a_input = torch.cat([h.repeat(1, N).view(N * N, -1), h.repeat(N, 1)], dim=1).view(N, -1, 2 * self.out_features)
-        e = self.leakyrelu(torch.matmul(a_input, self.a).squeeze(2))
+        # a_input = torch.cat([h.repeat(1, N).view(N * N, -1), h.repeat(N, 1)], dim=1).view(N, -1, 2 * self.out_features)
+        # e = self.leakyrelu(torch.matmul(a_input, self.a).squeeze(2))
+        f_1 = h @ self.a1
+        f_2 = h @ self.a2
+        e = self.leakyrelu(f_1 + f_2.transpose(0, 1))
 
-        zero_vec = -9e15*torch.ones_like(e)
+        zero_vec = -9e15 * torch.ones_like(e)
         attention = torch.where(adj > 0, e, zero_vec)
         attention = F.softmax(attention, dim=1)
         attention = F.dropout(attention, self.dropout, training=self.training)
@@ -43,11 +76,19 @@ class GraphAttentionLayer(nn.Module):
             return h_prime
 
     def __repr__(self):
-        return self.__class__.__name__ + ' (' + str(self.in_features) + ' -> ' + str(self.out_features) + ')'
+        return (
+            self.__class__.__name__
+            + " ("
+            + str(self.in_features)
+            + " -> "
+            + str(self.out_features)
+            + ")"
+        )
 
 
 class SpecialSpmmFunction(torch.autograd.Function):
     """Special function for only sparse region backpropataion layer."""
+
     @staticmethod
     def forward(ctx, indices, values, shape, b):
         assert indices.requires_grad == False
@@ -73,7 +114,7 @@ class SpecialSpmm(nn.Module):
     def forward(self, indices, values, shape, b):
         return SpecialSpmmFunction.apply(indices, values, shape, b)
 
-    
+
 class SpGraphAttentionLayer(nn.Module):
     """
     Sparse version GAT layer, similar to https://arxiv.org/abs/1710.10903
@@ -88,8 +129,8 @@ class SpGraphAttentionLayer(nn.Module):
 
         self.W = nn.Parameter(torch.zeros(size=(in_features, out_features)))
         nn.init.xavier_normal_(self.W.data, gain=1.414)
-                
-        self.a = nn.Parameter(torch.zeros(size=(1, 2*out_features)))
+
+        self.a = nn.Parameter(torch.zeros(size=(1, 2 * out_features)))
         nn.init.xavier_normal_(self.a.data, gain=1.414)
 
         self.dropout = nn.Dropout(dropout)
@@ -97,7 +138,7 @@ class SpGraphAttentionLayer(nn.Module):
         self.special_spmm = SpecialSpmm()
 
     def forward(self, input, adj):
-        dv = 'cuda' if input.is_cuda else 'cpu'
+        dv = "cuda" if input.is_cuda else "cpu"
 
         N = input.size()[0]
         edge = adj.nonzero().t()
@@ -114,7 +155,9 @@ class SpGraphAttentionLayer(nn.Module):
         assert not torch.isnan(edge_e).any()
         # edge_e: E
 
-        e_rowsum = self.special_spmm(edge, edge_e, torch.Size([N, N]), torch.ones(size=(N,1), device=dv))
+        e_rowsum = self.special_spmm(
+            edge, edge_e, torch.Size([N, N]), torch.ones(size=(N, 1), device=dv)
+        )
         # e_rowsum: N x 1
 
         edge_e = self.dropout(edge_e)
@@ -123,7 +166,7 @@ class SpGraphAttentionLayer(nn.Module):
         h_prime = self.special_spmm(edge, edge_e, torch.Size([N, N]), h)
         assert not torch.isnan(h_prime).any()
         # h_prime: N x out
-        
+
         h_prime = h_prime.div(e_rowsum)
         # h_prime: N x out
         assert not torch.isnan(h_prime).any()
@@ -136,4 +179,11 @@ class SpGraphAttentionLayer(nn.Module):
             return h_prime
 
     def __repr__(self):
-        return self.__class__.__name__ + ' (' + str(self.in_features) + ' -> ' + str(self.out_features) + ')'
+        return (
+            self.__class__.__name__
+            + " ("
+            + str(self.in_features)
+            + " -> "
+            + str(self.out_features)
+            + ")"
+        )
